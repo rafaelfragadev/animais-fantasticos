1bbd516b39354da3a3975860820e626d337014cf
Rafael Fraga <rafaelfragawebdesigner@gmail.com>
2022-07-03 15:31:06 -0300

    primeiro-commit

---
+++ js/modules/outsideclick.js
@@ -0,0 +1,20 @@
+export default function outsideClick(element, events, callback) {
+  const outside = 'data-outside';
+  const html = document.documentElement;
+
+  function handleOutsideClick(event){
+    if(!element.contains(event.target)) {
+    events.forEach(userEvent => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      });
+    element.removeAttribute(outside);
+    callback();
+      }
+    }
+  if(!element.hasAttribute(outside)){
+    events.forEach(userEvent => {
+      setTimeout(()=> html.addEventListener(userEvent, handleOutsideClick));
+    });
+    element.setAttribute(outside, '');
+    }
+  }
