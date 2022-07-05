a74a7aea6e3c9ad515014ac3444636459202c0ce
Rafael Fraga <rafaelfragawebdesigner@gmail.com>
2022-07-05 11:28:14 -0300

    fetch bitcoin refatorar

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,9 +1,9 @@
-export default function initFetchBitcoin() {
+export default function FetchBitcoin(url, target) {
 
-fetch('https://www.blockchain.com/ticker')
+fetch(url)
 .then(response => response.json())
 .then(bitcoin => {
-  const btcPreco = document.querySelector('.btc-preco');
+  const btcPreco = document.querySelector(target);
   btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
 }).catch(erro => {
   console.log(Error(erro));
@@ -12,3 +12,5 @@ fetch('https://www.blockchain.com/ticker')
 }
 // https://www.blockchain.com/ticker
 
+'https://www.blockchain.com/ticker'
+'.btc-preco'
